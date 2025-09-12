6b9f26c95fdf48235d98e6197c924e36908282e0
Sebastian Moeller <smoeller@dpz.eu>
2025-09-12 11:57:54 +0200

    Parse .sessionID file and make the output a bit nore useful

--- fn_parse_CCF_data.m
+++ fn_parse_CCF_data.m
@@ -22,14 +22,17 @@ debug = 0;
 if ~exist('CCF_run_folder_FQN_list', 'var') || isempty(CCF_run_folder_FQN_list)
 	% (venv_py3.10) root@LC38836:/home/smoeller@dpz.lokal/SCP_CODE/TASKS/foraging_task_2_NHP/src#
 	CCF_recordings_folder_FQN = fullfile('~', 'SCP_CODE', 'TASKS', 'foraging_task_2_NHP', 'recordings');
-	cur_CCF_runfolder_FQN_list = fullfile(CCF_recordings_folder_FQN, '001_101', '3');
+	cur_CCF_runfolder_FQN_list = fullfile(CCF_recordings_folder_FQN, '101_000', '0');
+	%cur_CCF_runfolder_FQN_list = fullfile(CCF_recordings_folder_FQN, '101_000', '0');
+	cur_CCF_runfolder_FQN_list = fullfile(CCF_recordings_folder_FQN, '102_000', '4');
+	cur_CCF_runfolder_FQN_list = fullfile(CCF_recordings_folder_FQN, '109_002', '0');
 
-	CCF_data_base_path = fullfile('/', 'Users', 'smoeller', 'DPZ', 'taskcontroller', 'CODE', 'CCF', 'CCF_RECORDINGS', 'recordings');
-	CCF_data_base_path = fullfile('/', 'Volumes', 'snd', 'taskcontroller', 'SCP_DATA', 'SCP-CTRL-01', 'CCF', 'recordings');
-	cur_CCF_runfolder_FQN_list = fullfile(CCF_data_base_path, '001_101', '28');
-	cur_CCF_runfolder_FQN_list = fullfile(CCF_data_base_path, '100_101', '3');
-
-	cur_CCF_runfolder_FQN_list = fullfile('/', 'Volumes', 'snd', 'taskcontroller', 'SCP_DATA', 'SCP-CTRL-01', 'CCF', 'recordings', '001_100', '7');
+	% CCF_data_base_path = fullfile('/', 'Users', 'smoeller', 'DPZ', 'taskcontroller', 'CODE', 'CCF', 'CCF_RECORDINGS', 'recordings');
+	% CCF_data_base_path = fullfile('/', 'Volumes', 'snd', 'taskcontroller', 'SCP_DATA', 'SCP-CTRL-01', 'CCF', 'recordings');
+	% cur_CCF_runfolder_FQN_list = fullfile(CCF_data_base_path, '001_101', '28');
+	% cur_CCF_runfolder_FQN_list = fullfile(CCF_data_base_path, '100_101', '3');
+	% 
+	% cur_CCF_runfolder_FQN_list = fullfile('/', 'Volumes', 'snd', 'taskcontroller', 'SCP_DATA', 'SCP-CTRL-01', 'CCF', 'recordings', '001_100', '7');
 
 	% use a file picker to select the desired folder
 end
@@ -46,6 +49,7 @@ for i_runfolder = 1 : length(cur_CCF_runfolder_FQN_list)
 	json_dir_struct = dir(fullfile(cur_CCF_runfolder_FQN, '*.json'));
 	h5_dir_struct = dir(fullfile(cur_CCF_runfolder_FQN, '*.h5'));
 	txt_dir_struct = dir(fullfile(cur_CCF_runfolder_FQN, '*.txt'));
+	sessionID_dir_struct = dir(fullfile(cur_CCF_runfolder_FQN, '*.sessionID'));
 
 	% the json files
 	for i_json_FQN = 1 : length(json_dir_struct)
@@ -63,6 +67,17 @@ for i_runfolder = 1 : length(cur_CCF_runfolder_FQN_list)
 			disp([cur_json_name, ' contained no data, skipping...']);
 		end
 	end
+
+	% load the sessionID
+	session_id = [];
+	if ~isempty(sessionID_dir_struct)
+		session_id = extractBefore(sessionID_dir_struct.name, '.sessionID');
+		CCF_session_dir_FQN = fileread(fullfile(sessionID_dir_struct.folder, sessionID_dir_struct.name));
+		CCF_session_dir_FQN_elements = split(strtrim(CCF_session_dir_FQN), filesep);
+		CCF_run = CCF_session_dir_FQN_elements{end};
+		CCF_pair = CCF_session_dir_FQN_elements{end-1};
+	end
+
 
 	for i_h5_FQN = 1 : length(h5_dir_struct)
 		cur_h5_name = h5_dir_struct(i_h5_FQN).name;
@@ -85,7 +100,7 @@ for i_runfolder = 1 : length(cur_CCF_runfolder_FQN_list)
 		end
 	end
 
-	if ~isempty(h5_struct) && length(fieldnames(h5_struct)) == 1 && ismember(fieldnames(h5_struct), {'record_data'})
+	if ~isempty(h5_struct) && ismember({'record_data'}, fieldnames(h5_struct))
 		% create a proper header for the data and reshape to 2D table...
 		data_struct.header = {};
 		[dim1, dim2, dim3] = size(h5_struct.record_data);	% dim1 X, Y, additional data, dim2; entries: 
@@ -107,23 +122,77 @@ for i_runfolder = 1 : length(cur_CCF_runfolder_FQN_list)
 			data_struct.header(end+1) = {['target_', num2str(cur_target_id), '_Y_rel']};
 			data_struct.header(end+1) = {['target_', num2str(cur_target_id), '_ID']};
 		end
+		
 	else
-		disp(['No data found in ', cur_CCF_runfolder_FQN]);
+		disp(['No data record_data found in ', cur_CCF_runfolder_FQN]);
+	end
+
+	if ~isempty(h5_struct) && ismember({'record2D_data'}, fieldnames(h5_struct))
+		% create a proper header for the data and reshape to 2D table...
+		data_struct.header = json_struct.record2D_header.record2D_column_names';
+		data_struct.table = squeeze(h5_struct.record2D_data)';
+
+		% data_struct.header = {};
+		% [dim1, dim2, dim3] = size(h5_struct.record_data);	% dim1 X, Y, additional data, dim2; entries: 
+		% n_cols = dim1 * dim2;
+		% n_rows = dim3;
+		% 
+		% data_struct.table = reshape(h5_struct.record_data, n_cols, n_rows)';
+		% data_struct.header = {'aim_0_X_rel', 'aim_0_Y_rel', 'tick_timestamp_sec', 'aim_1_X_rel', 'aim_1_Y_rel', '', 'agent_0_X', 'agent_0_Y', 'cumulative_score_0', 'agent_1_X', 'agent_1_Y', ''};
+		% n_agent_cols = 12;
+		% n_cols_per_entity = 3;
+		% n_targets = (n_cols - n_agent_cols)/3;
+		% for i_targets = 1 : n_targets
+		% 	cur_target_id_col_idx = (n_agent_cols + ((i_targets - 1) * n_cols_per_entity) + n_cols_per_entity);
+		% 	cur_target_id = unique(data_struct.table(:, cur_target_id_col_idx));
+		% 	if length(cur_target_id) ~= 1
+			% 	error('flattening 3D table failed, please investigate why');
+		% 	end
+		% 	data_struct.header(end+1) = {['target_', num2str(cur_target_id), '_X_rel']};
+		% 	data_struct.header(end+1) = {['target_', num2str(cur_target_id), '_Y_rel']};
+		% 	data_struct.header(end+1) = {['target_', num2str(cur_target_id), '_ID']};
+		% end
+		
+	else
+		disp(['No record2D data found in ', cur_CCF_runfolder_FQN]);
 	end
 
 	% now calculate the distances between the entities and add to table or
 	% add as new table
 
 	if ~isempty(h5_struct)
-		% quick and dirty reward
-		reward_differences = diff(data_struct.table(:, ismember(data_struct.header, {'cumulative_score_0'})));
-		[unique_reward_magnitudes, ~, unique_reward_magnitudes_row_idx] = unique(reward_differences);
+		% quick and dirty reward and collection estimation
+		% these are co9rrected for the offset if diff()
+		A0.collection_magnitude_tick_ldx = [false; diff(data_struct.table(:, ismember(data_struct.header, {'agent0_cumulative_score'})))];
+		B1.collection_magnitude_tick_ldx = [false; diff(data_struct.table(:, ismember(data_struct.header, {'agent1_cumulative_score'})))];
+		A0.collection_ldx = A0.collection_magnitude_tick_ldx > 0;
+		B1.collection_ldx = B1.collection_magnitude_tick_ldx > 0;
+		A0.n_collections = sum(A0.collection_ldx);
+		B1.n_collections = sum(B1.collection_ldx);
+		A0.n_pulses = sum(A0.collection_magnitude_tick_ldx);
+		B1.n_pulses = sum(B1.collection_magnitude_tick_ldx);
+		
+		% THIS is WRONG, as it only reports for agent0, needs a proper per
+		% target report
+		[unique_reward_magnitudes, ~, unique_reward_magnitudes_row_idx] = unique(A0.collection_magnitude_tick_ldx);
 		% count the occurrances
-		total_collections = sum(reward_differences >= 1);
+		total_collections = data_struct.table(end, ismember(data_struct.header, {'n_finished_collections'}));
+		total_duration_s = data_struct.table(end, ismember(data_struct.header, {'timestamp'})) - data_struct.table(1, ismember(data_struct.header, {'timestamp'}));
+
+		disp(['sessionID: ', session_id, '; CCF pair code: ', CCF_pair, '; CCF run number: ', CCF_run]);
+		disp(['duration [sec]: ', num2str(total_duration_s, '%0.0f'), '; total collections: ', num2str(total_collections), '; CA: ', num2str(A0.n_collections), '; CB: ', num2str(B1.n_collections), '; pulses: RA: ', num2str(A0.n_pulses), '; RB: ', num2str(B1.n_pulses)]);
+
+
+
+		% add a per target report: target ID target type, n collections (A:,
+		% B), n_rewards (A, B)
+
 
 		% Note, this will fail for different targets with equal reward
 		% magnitude...
 		% for this we need to calcuulate distances between agents and targets
+
+		% TODO replace by iterating over targets
 		for i_unique_rewards = 1 : length(unique_reward_magnitudes)
 			cur_reward_magnitude = unique_reward_magnitudes(i_unique_rewards);
 			%disp(['Reward magnitude ', num2str(cur_reward_magnitude)]);
